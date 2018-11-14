174f40ede73ef1912d4d4b02ee13b382a57c4f72
Kat Maldon <katharinamaldon@gmail.com>
2018-11-14 16:38:42 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -19,8 +19,9 @@ def greeting(greeting, name)
   return "#{greeting} #{name}"
 end
 
-def return_a_value
-
+def return_a_value(value)
+  value = "Nice"
+  return value 
 end
 
 def last_evaluated_value
