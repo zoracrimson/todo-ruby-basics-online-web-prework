7f8996a888c9caa40c12742e33b75d2b0c5f467a
Kat Maldon <katharinamaldon@gmail.com>
2018-11-14 16:45:54 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -28,7 +28,7 @@ def last_evaluated_value
   return "expert"
 end
 
-def pizza_party(topping)
-  topping = "cheese"
+def pizza_party(topping = "cheese")
+  topping = "pepperoni"
   return topping
 end
